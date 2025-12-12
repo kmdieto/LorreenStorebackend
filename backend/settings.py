0aa05b5b4fe248897ffed047f3b1dbb58c55b358
Kelvin Dieto <41842398+kmdieto@users.noreply.github.com>
2025-12-12 03:36:44 +0300

    Fix CORS URL formatting

--- backend/settings.py
+++ backend/settings.py
@@ -99,6 +99,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://693864fcb3f1a70008b23a4b--lorreenecomerce.netlify.app",
+    "https://lorreenecomerce.netlify.app",
 ]
+
 CORS_ALLOW_CREDENTIALS = True
